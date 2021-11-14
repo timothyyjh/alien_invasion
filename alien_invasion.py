e1a879158c52b7e5c0b92487d8b5a8b761a0c971
Timothy Yeo <timothyyjh@gmail.com>
2021-11-14 23:06:23 +0800

    Added ship configurations module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import sys		# To exit the game when the player quits
 import pygame	# To allow game functionalities
 from settings import Settings # To use the settings from another module.
-
+from ship import Ship
 
 class AlienInvasion:
 	"""Overall class to manage game assets and behavior."""
@@ -16,11 +16,15 @@ class AlienInvasion:
 		# Argument (1200, 800) is a tuple to define the dimensions of the game
 		# window. It is assigned to self.screen so it is accesible to all
 		# methods in the class.
-		self.screen = pygame.display.set_mode((self.settings.screen_width,
+		self.screen = pygame.display.set_mode((self.settings.screen_width, 
 		self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 
 		self.bg_color = (self.settings.bg_color)
+		# Ship requires one argument, an instance of AlienInvasion. This is the 
+		# parameter that gives Ship access to the game's parameters such as
+		# the screen property.
+		self.ship = Ship(self)
 
 	def run_game(self):
 		"""Start the main loop for the game."""
@@ -35,6 +39,7 @@ class AlienInvasion:
 			pygame.display.flip()
 
 			self.screen.fill(self.bg_color)
+			self.ship.blitme() # This is to draw the ship onto the screen.
 
 if __name__ == '__main__':
 	# Makes a game instance and runs the game.
