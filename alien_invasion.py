120f9f5c0c3f1d8fd72930da952f65008b7f0928
Timothy Yeo <timothyyjh@gmail.com>
2021-11-14 14:59:46 +0800

    Initialized alien_invasion core and settings module

---
+++ alien_invasion.py
@@ -0,0 +1,42 @@
+import sys		# To exit the game when the player quits
+import pygame	# To allow game functionalities
+from settings import Settings # To use the settings from another module.
+
+
+class AlienInvasion:
+	"""Overall class to manage game assets and behavior."""
+
+	def __init__(self):
+		"""Initializes the game and creates its resources."""
+		# Initializes the pygame module's background settings for the game to
+		# work properly.
+		pygame.init()
+		self.settings = Settings()
+
+		# Argument (1200, 800) is a tuple to define the dimensions of the game
+		# window. It is assigned to self.screen so it is accesible to all
+		# methods in the class.
+		self.screen = pygame.display.set_mode((self.settings.screen_width,
+		self.settings.screen_height))
+		pygame.display.set_caption("Alien Invasion")
+
+		self.bg_color = (self.settings.bg_color)
+
+	def run_game(self):
+		"""Start the main loop for the game."""
+		while True:
+			# Watches for keyboard and mouse events.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			# To retrieve the latest screen update everytime this while loop
+			# is run through.
+			pygame.display.flip()
+
+			self.screen.fill(self.bg_color)
+
+if __name__ == '__main__':
+	# Makes a game instance and runs the game.
+	ai = AlienInvasion()
+	ai.run_game()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""Where all the settings of Alien Invasion are stored."""
+
+	def __init__(self):
+		"""Initializes the game's settings."""
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
